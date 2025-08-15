89f9e27c4833b896f7bcf781d9f48956ff683e58
Sazid Ahammad <144606145+sazid123-max@users.noreply.github.com>
2025-08-15 20:02:49 +0600

    Create Add two numbers.C

---
+++ Add two numbers.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+float num1, num2, sum;
+printf("Enter first number: ");
+scanf("%f", &num1);
+printf("Emter second number: ");
+scanf("%f", &num2);
+sum = num1 + num2;
+printf("sum = %.f\n", sum);
+return 0;
+}
